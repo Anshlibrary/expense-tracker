cacdeb8cdc1464bd288a0bc620252dfbc9f0a1d1
Anshul Kashyap <kashyapanshul299@gmail.com>
2026-05-17 00:21:18 -0400

    Fixed reports and export routes

--- app.py
+++ app.py
@@ -14,6 +14,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+with app.app_context():
+    db.create_all()
+
 class Expense(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
